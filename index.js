ad1e2332bcc78dd609a32eaec76f9b81183d7dd8
Eddie <eddie@users-MacBook-Pro.local>
2021-11-03 17:57:45 -0700

    Unit test suite

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello World")
+const sumAnyNumbers = (...numbers) => {
+  let total = 0;
+
+  for (let number of numbers) {
+    if (isNaN(number) || number < 0) {
+      throw new Error("Sorry buddy only positive numbers");
+    }
+
+    let current = total + number;
+
+    if (current > 300) {
+      break;
+    }
+
+    total = current;
+  }
+
+  return total;
+};
+
+module.exports = {
+  sumAnyNumbers,
+};
--- index.test.js
+++ index.test.js
@@ -1,6 +1,35 @@
-describe("Math", () => {
-    it ("Sum", () =>{
-        expect(2+2).toBe(4)
-    })
+const { sumAnyNumbers } = require("./index");
 
-})
+describe("Math", () => {
+  describe("Sum", () => {
+    it("should make sure it sums correctly", () => {
+      expect(sumAnyNumbers(2, 2)).toBe(4);
+    });
+
+    it("should not allow negative numbers", () => {
+      function sumTwoErrorTest() {
+        sumAnyNumbers(2, -2);
+      }
+      expect(sumTwoErrorTest).toThrowError(
+        new Error("Sorry buddy only positive numbers")
+      );
+    });
+
+    it("not accept any other type but numbers", () => {
+      function sumTwoErrorTest() {
+        sumAnyNumbers([], () => {});
+      }
+      expect(sumTwoErrorTest).toThrowError(
+        new Error("Sorry buddy only positive numbers")
+      );
+    });
+
+    it("should only accept numbers", () => {
+      expect(sumAnyNumbers(2, 3, 4, 5, 6)).toBe(20);
+    });
+
+    it("should only sum up to 300, if not overlap", () => {
+      expect(sumAnyNumbers(100, 100, 50, 100, 100, 100)).toBe(250);
+    });
+  });
+});
